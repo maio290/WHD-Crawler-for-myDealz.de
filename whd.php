efbe29f0df0bcd8c59455501819e946b9c2eac89
maio290 <maio290@maio290.de>
2015-08-01 03:44:55 +0200

    Fixed UTF-8 Encoding & HTML Header

--- whd.php
+++ whd.php
@@ -26,13 +26,21 @@ require_once("idealo.php");
 	$urlm = prepare_url_mobile($asin);
 	$urld = prepare_url_desktop($asin);
 	$data_arr = get_data($urlm);
+	echo '<!DOCTYPE html>
+		<html lang="de">
+		<head>
+		<meta charset="utf-8">
+		<title> mydealz.de - WHD Crawler</title>
+		</head>
+		<body>
+		  ';
 	echo '<textarea rows="20" cols="50">';
 	echo '[url='.$urld.']';
 	echo '[img]'.$data_arr[2].'[/img]';
 	echo "\n";
 	echo $data_arr[0];
-	echo "\n";
-	echo "\n";
+	echo "\n
+	      \n";
 	
 	// Convert to number in order to compare it properly
 	// 4 = names, 5 = prices (idealo)
@@ -57,7 +65,7 @@ require_once("idealo.php");
 				$price_idealo_arr = $data_arr[5];
 				$price_idealo = strip_tags($price_idealo_arr[0]);
 				$price_idealo = substr($price_idealo,0,strpos($price_idealo, "-"));
-				$price_idealo = str_replace("€","EUR",$price_idealo);
+				$price_idealo = str_replace("А","EUR",$price_idealo);
 				$name_idealo = substr($name_idealo, (strpos($name_idealo, ">",1)+1), -1);
 				$name_idealo = strip_tags($name_idealo);
 				$name_idealo = str_replace(20,"",$name_idealo);
@@ -97,7 +105,7 @@ require_once("idealo.php");
 				$name_idealo = substr($name_idealo, (strpos($name_idealo, ">",1)+1), -1);
 				$name_idealo = strip_tags($name_idealo);
 				$name_idealo = str_replace(20,"",$name_idealo);
-				$name_idealo = preg_replace('/[^a-zA-Z0-9_ ßäöü%\[\]\.\(\)%&-]/s', '', $name_idealo);
+				$name_idealo = preg_replace('/[^a-zA-Z0-9_äöüß%\[\]\.\(\)%&-]/s', '', $name_idealo);
 				echo ("Idealo (".$name_idealo."): " . $price_idealo);
 				echo "\n";
 				}
@@ -106,6 +114,8 @@ require_once("idealo.php");
 	
 	
 	}
+		echo '</body>
+		</html>';
 	}
 function prepare_url_mobile($item_id)
 {
@@ -125,9 +135,10 @@ if(strpos($url,"/gp/product/") != false)
 function get_data($url) {
 // Use with mobile links only
 get_data:
-$html = file_get_html($url)->plaintext;
-$name =  substr($html, 0, strpos($html, "Amazon.de")-1);
-$name = trim($name," ");
+$html = file_get_html($url);
+$name = $html ->find('b[id="product-title"]');
+$name = $name[0];
+$name = strip_tags($name);
 // Priceurl: http://www.amazon.de/gp/aw/ol/B00OBIXRSY?o=Used&op=1
 // Common Url: http://www.amazon.de/gp/aw/d/B00OBIXRSY 
 // MerchantID: A8KICS1PHF7ZO
@@ -157,7 +168,7 @@ refetch_outer:
 $pricehtml = file_get_html($priceurl);
 		if($pricehtml == false)
 		{
-				goto refetch_outer;
+				//goto refetch_outer;
 		}
 $pricehtml2 = $pricehtml ->find('a[href]');
 
@@ -269,7 +280,7 @@ $finalimgurl = str_replace('"','', $imgurl2[0]);
 
 
 $pvl = get_idealo($name);
-
+$name = utf8_encode($name);
 
 $arr = [
 0 => $name,
